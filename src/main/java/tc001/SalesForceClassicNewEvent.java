a837062669bfecf6d1d8bfe3bb01aeea9362fc77
Selvapriya A <47075@hexaware.com>
2021-05-10 12:02:48 +0530

    first commit

--- src/main/java/tc001/SalesForceClassicNewEvent.java
+++ src/main/java/tc001/SalesForceClassicNewEvent.java
@@ -12,7 +12,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SalesForceClassicNewEvent {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
@@ -24,19 +24,21 @@ public class SalesForceClassicNewEvent {
 		driver.findElementById("username").sendKeys("matschie@testleaf.com");
 		driver.findElementById("password").sendKeys("India@123");
 		driver.findElementById("Login").click();
+		Thread.sleep(3000);
 		driver.findElementByXPath("//div[@data-aura-class='forceEntityIcon'][1]").click();
 		driver.findElementByXPath("//a[text()='Switch to Salesforce Classic']").click();
-		driver.findElementByXPath("//a[text()='Dashboards']").click();
+		Thread.sleep(3000);
+		driver.findElementByXPath("//li[@id='Dashboard_Tab']").click();
 		driver.findElementByXPath("//a[text()='Go to Dashboard List']").click();
 		driver.findElementByXPath("//input[@title='New Dashboard...']").click();
 		List<WebElement> columnsize = driver.findElementsByXPath("//button[text()= 'Medium']");
 		for(WebElement medium : columnsize)
 			medium.click();
-		driver.findElementByXPath("//span[text()='Wide']").click(); 
-		WebElement drag = driver.findElementByXPath("//img[@alt='Pie Chart']");
-		WebElement drop = driver.findElementByXPath("//div[@id='ext-comp-1064']");
+		driver.findElementByXPath("(//span[text()='Wide'])[3]").click(); 
 		Actions builder = new Actions(driver);
-		builder.dragAndDrop(drag, drop);
+		WebElement drag = driver.findElementByXPath("//img[@alt='Pie Chart']");
+		WebElement drop = driver.findElementByXPath("//*[@id=\"ext-gen137\"]");
+		builder.dragAndDrop(drag, drop).perform();
 		
 		
 
